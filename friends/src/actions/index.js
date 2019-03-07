6de3ddabf0190a02407937cb71b6a17e67b70fe3
korynewton <korynewtondev@gmail.com>
2019-03-07 14:46:15 -0800

    starting friend component

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,17 +1,29 @@
 import axios from 'axios'
 export const LOGGING_IN = "LOGGING_IN"
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
+export const FETCHING_FRIENDS = "FETCHING_FRIENDS"
+export const SAVING_FRIENDS = "SAVING_FRIENDS"
+
+
 
 export const login = creds => dispatch => {
-    console.log("button pressed", creds)
     dispatch({ type : LOGGING_IN });
     return axios.post('http://localhost:5000/api/login', creds)
         .then(res => {
+            dispatch({ type: LOGIN_SUCCESS })
             localStorage.setItem('token', res.data.payload)
         })
         .catch(err => console.log("error: ", err))
 }
-    //     localStorage.setItem('token', res.data.payload);
-    //     dispatch({ type: 'LOGIN_SUCCESS', payload: res.data.payload });
-    //   });
-// }
+
+export const retrieveFriends = () => dispatch => {
+    dispatch({ type: FETCHING_FRIENDS })
+    axios.get("http://localhost:5000/api/friends", { 
+        headers: { Authorization: localStorage.getItem('token')}
+    })
+    .then(res => {
+        console.log("response", res.data)
+    })
+    .catch(err => console.log(err))
+}
+   
