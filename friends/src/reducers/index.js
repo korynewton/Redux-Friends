3adc3746c73b28f50eff23c058e366700a9c57fa
korynewton <korynewtondev@gmail.com>
2019-03-07 12:10:15 -0800

    project initialized, basic rootReducer in place

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+    friends: []
+}
+
+export const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+
+}
