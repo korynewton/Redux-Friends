e326df4ceaa3f74b035e137abf2d40c08c527460
korynewton <korynewtondev@gmail.com>
2019-03-07 13:23:30 -0800

    login is working and storing auth to localStorage

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -3,11 +3,13 @@ export const LOGGING_IN = "LOGGING_IN"
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
 
 export const login = creds => dispatch => {
-    console.log("button pressed")
+    console.log("button pressed", creds)
     dispatch({ type : LOGGING_IN });
-    axios.post('http://localhost:5000/api/login', creds)
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
+    return axios.post('http://localhost:5000/api/login', creds)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload)
+        })
+        .catch(err => console.log("error: ", err))
 }
     //     localStorage.setItem('token', res.data.payload);
     //     dispatch({ type: 'LOGIN_SUCCESS', payload: res.data.payload });
