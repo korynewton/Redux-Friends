13aa17226f95af94e820b7b2334998c535ed5c00
korynewton <korynewtondev@gmail.com>
2019-03-07 12:19:01 -0800

    brought in logger and passing store in as props on Provider component

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,14 +4,15 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { rootReducer } from './reducers'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
-    <Provider>
+    <Provider store = {store}>
         <App />
     </Provider>, document.getElementById('root'));
 
